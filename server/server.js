2094190df77f6075ca530a579cb27d3c7902a407
JohnsonHStephen <johnsonh.stephen@gmail.com>
2019-04-04 19:09:43 -0400

    removed https (I dont want to buy an ssl certificate)

--- server/server.js
+++ server/server.js
@@ -3,13 +3,6 @@ const hbs       = require('express-handlebars');
 const path      = require('path');
 const mongoose  = require('mongoose');
 const request   = require('request');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 
 
 mongoose.connect('mongodb://localhost:27017/myWebsite', { useNewUrlParser: true, useCreateIndex: true, autoIndex: false });
@@ -47,58 +40,55 @@ app.use('/', require('./routes/indexRoute'));
 app.use('/resume', require('./routes/resumeRoute'));
 app.use('/portfolio', require('./routes/portfolioRoute'));
 
-var httpServer = http.createServer(app).listen(PORT);
-var httpsServer = https.createServer(credentials, app).listen(PORT + 443);
+app.listen(PORT, err => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(`Server listening on port: ${PORT}`);
+    console.log(`Open at:`);
+    console.log(`http://localhost:3000`);
 
-// app.listen(PORT, err => {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(`Server listening on port: ${PORT}`);
-//     console.log(`Open at:`);
-//     console.log(`http://localhost:3000`);
-//
-//     var options = {
-//       url: 'https://api.github.com/orgs/StephenHJohnson/repos',
-//       headers: {
-//         'User-Agent': 'JohnsonHStephen'
-//       }
-//     };
-//
-//     request.get(options, function(error, response, body) {
-//       if(error) {
-//         console.log("Error reading from GitHub API: ", error);
-//       } else {
-//         let Project = require('./models/project');
-//
-//         // console.log(JSON.parse(body));
-//         Project.deleteMany({}, function(e, result){
-//           if(e) {
-//             console.log("Error clearing mongodb myWebsite: ", e);
-//           } else {
-//             console.log("Clearing old projects.");
-//             for(var project of JSON.parse(body)) {
-//               if(project.language === "JavaScript" && project.name !== "Website") {
-//                 var temp = new Project({
-//                   name: project.name,
-//                   gitUrl: project.html_url,
-//                   description: project.description,
-//                   updated: project.pushed_at,
-//                   imagePath: project.owner.avatar_url
-//                 });
-//
-//                 temp.save(function(e, result) {
-//                   if(e) {
-//                     console.log("Error saving to mongodb myWebsite: ", e);
-//                   } else {
-//                     console.log("inserted", temp);
-//                   }
-//                 });
-//               }
-//             }
-//           }
-//         });
-//       }
-//     });
-//   }
-// });
+    var options = {
+      url: 'https://api.github.com/orgs/StephenHJohnson/repos',
+      headers: {
+        'User-Agent': 'JohnsonHStephen'
+      }
+    };
+
+    request.get(options, function(error, response, body) {
+      if(error) {
+        console.log("Error reading from GitHub API: ", error);
+      } else {
+        let Project = require('./models/project');
+
+        // console.log(JSON.parse(body));
+        Project.deleteMany({}, function(e, result){
+          if(e) {
+            console.log("Error clearing mongodb myWebsite: ", e);
+          } else {
+            console.log("Clearing old projects.");
+            for(var project of JSON.parse(body)) {
+              if(project.language === "JavaScript" && project.name !== "Website") {
+                var temp = new Project({
+                  name: project.name,
+                  gitUrl: project.html_url,
+                  description: project.description,
+                  updated: project.pushed_at,
+                  imagePath: project.owner.avatar_url
+                });
+
+                temp.save(function(e, result) {
+                  if(e) {
+                    console.log("Error saving to mongodb myWebsite: ", e);
+                  } else {
+                    console.log("inserted", temp);
+                  }
+                });
+              }
+            }
+          }
+        });
+      }
+    });
+  }
+});
